a3b76fac1a6427a707717019aceb67f0354bfbad
GokulBVMoback <113531158+GokulBVMoback@users.noreply.github.com>
2022-09-18 12:17:21 +0530

    Add files via upload

---
+++ passwordThreeWrong/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passwordThreeWrong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\t\t\t\t\tCheck Username and Password\n");
+            Console.WriteLine("\t\t\tBy Default Username is \"Gokul\" and Password is \"1234\"");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+            string username, password;
+            Console.Write("Enter User Name:");
+            username = Console.ReadLine();
+            Console.Write("Enter Your Password:");
+            password = Console.ReadLine();
+            if (username == "Gokul" && password == "1234")
+            {
+                Console.WriteLine("LogIn successfully."); //if username and password correct at first attempt
+            }
+            else    // else enter into the loop
+            {
+                for (int i = 1; i <=3; i++) //i intex start from 1 and i value increased by +1 for each failed attempt
+                {
+                    Console.WriteLine("Username Password doesn`t match. Please Re-Enter your Username and Password");
+                    Console.Write("\nUsername:");
+                    username = Console.ReadLine();
+                    Console.Write("Password:");
+                    password = Console.ReadLine();
+                    if (username == "Gokul" && password == "1234")
+                    {
+                        Console.WriteLine("LogIn successfully.");
+                        break;
+                    }
+                    else if (i == 3) //if i = 2 you can`t try any more. else it re-enter to the for loop
+                    {
+                        Console.WriteLine("Login attemp more than three times. Try later!");
+                    }
+                }
+                
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ passwordThreeWrong/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("passwordThreeWrong")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("passwordThreeWrong")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d222aa25-645b-4055-982d-af27b11e1b72")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
